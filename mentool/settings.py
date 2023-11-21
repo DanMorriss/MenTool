9171b4fe1c99f1ffb144c5a23e31c6f1434e8f39
Dan Morriss <danielmorriss1@gmail.com>
2023-11-21 21:56:00 +0000

    Wire up 'home' page

--- mentool/settings.py
+++ mentool/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "mentool.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
