beb8282198f518a4d8844b28daf19fa68c7973a7
Dan Morriss <danielmorriss1@gmail.com>
2023-11-21 19:35:56 +0000

    Add Mood model

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Mood(models.Model):
+
+    username = models.ForeignKey(User, on_delete=models.CASCADE,
+                                 related_name='username')
+    date = models.DateField()
+    comment = models.CharField(max_length=500, blank=True)
