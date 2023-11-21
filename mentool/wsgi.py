b5677f6d88ccef787c8f46a77966b9d7f093cf59
Tomislav Dukez <tomdu3@ymail.com>
2023-11-21 15:51:25 +0000

    Fix: correct project name

--- mentool/wsgi.py
+++ mentool/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mental project.
+WSGI config for mentool project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mental.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mentool.settings")
 
 application = get_wsgi_application()
