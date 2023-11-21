cd03b10b7829f2f825a3ffbe0610ccdec7636d0e
Dan Morriss <danielmorriss1@gmail.com>
2023-11-21 22:34:29 +0000

    Wire up all pages

--- tracker/views.py
+++ tracker/views.py
@@ -8,3 +8,23 @@ class HomeView(TemplateView):
 
 class MoodView(TemplateView):
     template_name = 'tracker/tracker_mood.html'
+
+
+class StatsView(TemplateView):
+    template_name = 'tracker/tracker_stats.html'
+
+
+class LandingView(TemplateView):
+    template_name = 'tracker/tracker_landing.html'
+
+
+class AboutUsView(TemplateView):
+    template_name = 'tracker/tracker_about_us.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'tracker/tracker_login.html'
+
+
+class SignUpView(TemplateView):
+    template_name = 'tracker/tracker_sign_up.html'
