467d7f67bd89c2ddf194d64e5bd1cb9c6e3c3fb6
Tomislav Dukez <tomdu3@ymail.com>
2023-11-22 14:30:00 +0000

    Add Mood class

--- tracker/models.py
+++ tracker/models.py
@@ -1,10 +1,26 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
-# class Mood(models.Model):
+class Mood(models.Model):
+    VERY_HAPPY = 0
+    HAPPY = 1
+    MEH = 2
+    SAD = 3
+    VERY_SAD = 4
+    MOOD_CHOICES = (
+        (VERY_HAPPY, 'Very Happy'),
+        (HAPPY, 'Happy'),
+        (MEH, 'Meh'),
+        (SAD, 'Sad'),
+        (VERY_SAD, 'Very Sad'),
+    )
 
-#     username = models.ForeignKey(User, on_delete=models.CASCADE,
-#                                  related_name='username')
-#     date = models.DateField()
-#     comment = models.CharField(max_length=500, blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='moods')
+    date= models.DateField()
+    mood_level = models.IntegerField(choices=MOOD_CHOICES, default=0)
+    comment = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+                return f'{self.user} - {self.date} - {self.get_mood_level_display()} - {self.comment}'
