9781a4c696351d9b4b340aa604dfe31c4e4f8887
Tomislav Dukez <tomdu3@ymail.com>
2023-11-23 13:29:58 +0000

    Add crispy-forms requirement and settings

--- mentool/settings.py
+++ mentool/settings.py
@@ -50,8 +50,11 @@ INSTALLED_APPS = [
     "cloudinary_storage",
     "django.contrib.staticfiles",
     "cloudinary",
+    "crispy_forms",
     "tracker",
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
